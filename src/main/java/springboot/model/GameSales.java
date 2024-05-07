913a8e898992a3c651c09117bf1b0427e73a7eb2
Song Lei <songlei198974@gmail.com>
2024-05-08 05:59:16 +0800

    a

--- src/main/java/springboot/model/GameSales.java
+++ src/main/java/springboot/model/GameSales.java
@@ -23,32 +23,32 @@ public class GameSales {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "sales_id")
+	@Column(name = "sales_id") // a running number starts with 1
 	private Long salesId;
 	
-	@Column(name = "game_no", length = 100)
+	@Column(name = "game_no")  // an integer value between 1 to 100
 	private int gameNo;
 	
-	@Column(name = "game_name", length = 20)
+	@Column(name = "game_name", length = 20) // a string value not more than 20 characters
 	private String gameName;
 	
-	@Column(name = "game_code", length = 5)
+	@Column(name = "game_code", length = 5) // a string value not more than 5 characters
 	private String gameCode;
 	
-	@Column(name = "type")
+	@Column(name = "type") // an integer, 1 = Online | 2 = Offline
 	private int type;
 	
 	@Column(name = "cost_price")
-	private BigDecimal costPrice;
+	private BigDecimal costPrice; // decimal value not more than 100
 	
 	@Column(name = "tax")
-	private BigDecimal tax = new BigDecimal("0.09");
+	private BigDecimal tax = new BigDecimal("0.09"); // 9%
 	
 	@Column(name = "sale_price")
-	private BigDecimal salesPrice;
+	private BigDecimal salesPrice; // decimal value, cost_price inclusive of tax
 	
 	@Column(name = "date_of_sale")
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date salesDate;
+	@Temporal(TemporalType.TIMESTAMP) 
+	private Date salesDate; // a timestamp of the sale
 
 }
