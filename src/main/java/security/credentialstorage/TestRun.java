83b1ae7cea2218c837dd0f5e8f5257a41e7751ea
Song Lei <songlei198974@gmail.com>
2024-10-04 10:08:11 +0800

    a

--- src/main/java/security/credentialstorage/TestRun.java
+++ src/main/java/security/credentialstorage/TestRun.java
@@ -1,4 +1,4 @@
-package security;
+package security.credentialstorage;
 
 import security.credentialstorage.implementation.windows.CredManagerBackedCredentialStore;
 import security.credentialstorage.model.StoredCredential;
